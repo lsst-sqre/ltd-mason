f6ffaf2223ca27b4b5c6711c429a8daccaed852a
Jonathan Sick <jsick@lsst.org>
2016-02-16 16:54:39 -0700

    ltd-mason cli takes --config argument
    The config file (~/.ltdmason.yaml) is envisioned as a YAML file with
    fields keeper_url and keeper_token containing the URL and an auth token
    for a ltd-keeper instance.
    
    For DM-4196.

--- ltdmason/cli.py
+++ ltdmason/cli.py
@@ -1,7 +1,7 @@
 """Command line interface for ltd-mason that runs a doc build."""
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
-from builtins import *
+from builtins import *  # NOQA
 from future.standard_library import install_aliases
 install_aliases()
 
@@ -12,13 +12,15 @@ from io import open
 import tempfile
 import shutil
 
+import ruamel.yaml
+
 from .manifest import Manifest
 from .product import Product
 
 
 def run_ltd_mason():
     """Command line app: ``ltd-mason``.
-    
+
     This app is intended to be called from ``buildlsstsw.sh`` and is
     responsible for running the full documentation build and upload
     process.
@@ -32,6 +34,8 @@ def run_ltd_mason():
         with open(args.manifest_path, mode='r', encoding='utf-8') as f:
             manifest_data = f.read()
     manifest = Manifest(manifest_data)
+
+    configs = read_configs(args.config_path)
 
     # FIXME replace with a context
     build_dir = tempfile.mkdtemp()
@@ -61,6 +65,13 @@ def parse_args():
             or piped in,
 
             cat manifest.yaml | ltd-mason
+
+            ltd-mason also expects a configuration file (for working with the
+            ltd-keeper documentation coordinater. The configuration file is
+            YAML-formatted with fields
+
+               keeper_url: '<URL of ltd-keeper instance>'
+               keeper_token: '<auth token from ltd-keeper instance>'
             """),
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog='See https://github.com/lsst-sqre/ltd-mason')
@@ -69,5 +80,29 @@ def parse_args():
         dest='manifest_path',
         default=None,
         help='Path to YAML manifest file that defines the doc build.')
+    parser.add_argument(
+        '--config',
+        dest='config_path',
+        help='Path to ltd-mason configuration file (default `~/.ltdmason`)')
     args, unknown_args = parser.parse_known_args()
     return args, unknown_args
+
+
+def read_configs(config_path):
+    """Read a YAML file with ltd-mason configurations."""
+    try:
+        with open(config_path, mode='r', encoding='utf-8') as f:
+            config_data = ruamel.yaml.load(f)
+    except OSError:
+        raise OSError('Could not read configuration file at {0}'.format(
+            config_path))
+
+    if 'keeper_url' not in config_data:
+        raise RuntimeError('No config {0} in {1}'.format(
+            'keeper_url', config_path))
+
+    if 'keeper_token' not in config_data:
+        raise RuntimeError('No config {0} in {1}'.format(
+            'keeper_token', config_path))
+
+    return config_data

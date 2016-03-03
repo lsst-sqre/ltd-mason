c8c739e1717bb1dba48ed1626cf06734cfc00d1b
Jonathan Sick <jsick@lsst.org>
2016-03-02 22:25:30 -0700

    Run a documentation build as a unit test
    Rather than relying on an lsstsw/ to test against, ltd-mason now tests
    against a very simple test documentation directory at
    github.com/lsst-sqre/mock-doc and the packages are embedded in this repo
    under test_data

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,76 +1,98 @@
+"""Test the Product build process using a mock documentation.
+
+See mock_manifest.yaml; the doc repo is github.com/lsst-sqre/mock-doc and
+the packages are embedded in this repo's test_data/ directory.
+"""
+
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 from builtins import *  # NOQA
 from future.standard_library import install_aliases
 install_aliases()
 
+import os
 import tempfile
 import shutil
-import os
 
 import pkg_resources
 import pytest
 import sh
+import ruamel.yaml
 
 from ltdmason.product import Product
 from ltdmason.manifest import Manifest
 
 
-@pytest.fixture
-def demo_science_pipelines_manifest():
-    resource_args = (__name__, 'demo_science_pipelines_manifest.yaml')
+@pytest.fixture(scope='session')
+def mock_manifest():
+    """Provide the mock_manifest.yaml test file as a string."""
+    resource_args = (__name__, 'mock_manifest.yaml')
     assert pkg_resources.resource_exists(*resource_args)
-    yaml_data = pkg_resources.resource_string(*resource_args)
-    return yaml_data
+    yaml_str = pkg_resources.resource_string(*resource_args)
+
+    # Patch the mock manifest to turn embedded package dir paths into
+    # absolute paths
+    yaml_data = ruamel.yaml.load(yaml_str)
+    pkg_names = ('alpha', 'beta')
+    test_dir = os.path.dirname(__file__)
+    for n in pkg_names:
+        p = os.path.join(test_dir, yaml_data['packages'][n]['dir'])
+        yaml_data['packages'][n]['dir'] = p
+    yaml_str = ruamel.yaml.dump(yaml_data)
+
+    return yaml_str
 
 
-def test_clone_doc_repo(demo_science_pipelines_manifest):
-    manifest = Manifest(demo_science_pipelines_manifest)
+@pytest.fixture(scope='session')
+def product(request, mock_manifest):
+    """Run the full product build pipeline.
 
-    # future doesn't have this?
-    # with tempfile.TemporaryDirectory() as build_dir:
-    #     product = Product(manifest, build_dir)
-    #     product.clone_doc_repo()
-    #     assert os.path.exists(product.doc_dir)
+    Since this fixture is session scoped it will only be run once; the tests
+    then inspect the built product.
 
+    The built documentation is automatically cleaned up with a finalizer.
+    """
     build_dir = tempfile.mkdtemp()
-
+    manifest = Manifest(mock_manifest)
     product = Product(manifest, build_dir)
     product.clone_doc_repo()
+    product.link_package_repos()
+    product.build_sphinx()
+
+    def cleanup():
+        shutil.rmtree(build_dir)
+    request.addfinalizer(cleanup)
+
+    return product
+
+
+def test_clone_doc_repo(product):
+    """Test how the doc repo was cloned."""
+    manifest = product.manifest
+
     assert os.path.exists(product.doc_dir)
     # verify the correct ref is checked out
     git = sh.git.bake(_cwd=product.doc_dir)
     ref = str(git('symbolic-ref', 'HEAD').strip())
-    print(ref)
-    print(manifest.doc_repo_ref)
+    print("doc repo ref:", ref, manifest.doc_repo_ref)
     assert ref.endswith(manifest.doc_repo_ref)
 
-    shutil.rmtree(build_dir)
 
-
-@pytest.mark.skipif(os.getenv('STACK_AFW') is None,
-                    reason='Requires $STACK_AFW to be set')
-def test_link_package_repos(demo_science_pipelines_manifest):
-    manifest = Manifest(demo_science_pipelines_manifest)
-
-    build_dir = tempfile.mkdtemp()
-
-    product = Product(manifest, build_dir)
-    product.clone_doc_repo()
-    product.link_package_repos()
-
+def test_link_package_repos(product):
+    """Test links made from doc repo to package repos."""
     print(sh.ls('-al', product.doc_dir))
 
     for package_name, package_data in product.manifest.packages.items():
         print(package_name)
         # test that the package's directory exists in docs
-        package_dir = os.path.join(build_dir, product.manifest.doc_repo_name,
+        package_dir = os.path.join(product.build_dir,
+                                   product.manifest.doc_repo_name,
                                    str(package_name))
         print(package_dir)
         assert os.path.isdir(package_dir)
 
         # test that the packages doc/_static directory is linked
-        package_static_dir = os.path.join(package_data['dirname'],
+        package_static_dir = os.path.join(package_data['dir'],
                                           'doc', '_static', package_name)
         if os.path.exists(package_static_dir):
             doc_package_static_dir = os.path.join(product.doc_dir,
@@ -80,7 +102,7 @@ def test_link_package_repos(demo_science_pipelines_manifest):
 
         # test that individual entities of a package's doc (aside from _static)
         # are linked
-        source_dir = os.path.join(package_data['dirname'], 'doc')
+        source_dir = os.path.join(package_data['dir'], 'doc')
         print('source_dir', source_dir)
         print(os.listdir(source_dir))
         target_dir = os.path.join(product.doc_dir, str(package_name))
@@ -92,21 +114,6 @@ def test_link_package_repos(demo_science_pipelines_manifest):
             assert os.path.islink(link_name)
             assert os.path.lexists(link_name)
 
-    shutil.rmtree(build_dir)
 
-
-@pytest.mark.skipif(os.getenv('STACK_AFW') is None,
-                    reason='Requires $STACK_AFW to be set')
-def test_sphinx_build(demo_science_pipelines_manifest):
-    manifest = Manifest(demo_science_pipelines_manifest)
-
-    build_dir = tempfile.mkdtemp()
-
-    product = Product(manifest, build_dir)
-    product.clone_doc_repo()
-    product.link_package_repos()
-    product.build_sphinx()
-
+def test_sphinx_build(product):
     assert os.path.exists(os.path.join(product.doc_dir, '_build', 'html'))
-
-    shutil.rmtree(build_dir)

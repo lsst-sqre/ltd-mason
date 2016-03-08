437077fd9749f0dc6b1149a3fde3aba885505048
Jonathan Sick <jsick@lsst.org>
2016-03-07 17:01:52 -0700

    Add tests for read_keeper_credentials()

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,7 +5,7 @@ from base64 import b64encode
 import responses
 import pytest
 
-from ltdmason.cli import get_keeper_token
+from ltdmason.cli import get_keeper_token, read_keeper_credentials
 
 
 @responses.activate
@@ -41,3 +41,37 @@ def test_get_keeper_token_error():
 
     with pytest.raises(RuntimeError):
         get_keeper_token('http://localhost:5000', 'user', 'pass')
+
+
+def test_read_keeper_credentials(monkeypatch):
+    """Basic test of read_keeper_credentials()"""
+    _url = 'http://ltd-keeper.lsst.codes'
+    _user = 'user'
+    _pass = 'pass'
+    monkeypatch.setenv('LTD_KEEPER_URL', _url)
+    monkeypatch.setenv('LTD_KEEPER_USER', _user)
+    monkeypatch.setenv('LTD_KEEPER_PASSWORD', _pass)
+
+    c = read_keeper_credentials()
+    assert c['keeper_url'] == _url
+    assert c['keeper_username'] == _user
+    assert c['keeper_password'] == _pass
+
+
+@pytest.mark.parametrize('missing', ['LTD_KEEPER_URL', 'LTD_KEEPER_USER',
+                                     'LTD_KEEPER_PASSWORD'])
+def test_read_keeper_credentials_missing(monkeypatch, missing):
+    """Test that read_keeper_credentials raises a RuntimeError when a necessary
+    environment variable is missing.
+    """
+    _url = 'http://ltd-keeper.lsst.codes'
+    _user = 'user'
+    _pass = 'pass'
+    monkeypatch.setenv('LTD_KEEPER_URL', _url)
+    monkeypatch.setenv('LTD_KEEPER_USER', _user)
+    monkeypatch.setenv('LTD_KEEPER_PASSWORD', _pass)
+
+    monkeypatch.delenv(missing)
+
+    with pytest.raises(RuntimeError):
+        read_keeper_credentials()

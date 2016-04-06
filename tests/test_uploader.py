e442fca7fd942f1a6ceab37be34a1d9b2296cbf2
Jonathan Sick <jsick@lsst.org>
2016-04-06 13:39:34 -0700

    Add test_upload_via_keeper using pytest-mock
    test_upload_via_keeper is a wrapper function. We already test the
    business logic on the functions it calls, but this commit finally
    ensures that test_upload_via_keeper is calling those functions properly.
    
    We're using mock (unittest.mock) to do this. However, I can't use
    straight mock decorators with py.test since they clash with the way
    py.test does fixtures.
    
    Thus we formally add pytest-mock==0.11.0 plugin to requirements.

--- tests/test_uploader.py
+++ tests/test_uploader.py
@@ -1,11 +1,16 @@
 """Tests for the ltdmason/uploader module."""
 
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 import responses
 import pkg_resources
 import pytest
 
 from ltdmason.manifest import Manifest
-from ltdmason.uploader import _register_build, _confirm_upload, KeeperError
+from ltdmason.uploader import (_register_build, _confirm_upload, KeeperError,
+                               upload_via_keeper)
 
 
 @pytest.fixture
@@ -30,6 +35,7 @@ def test_register_build_ok(demo_manifest):
         "product_url": "http://localhost:5000/products/lsst_apps",
         "self_url": "http://localhost:5000/builds/1",
         "slug": "b1",
+        "surrogate_key": "35d7a50a1d1b40ab9e7a56cd169f356e",
         "uploaded": False}
     responses.add(
         responses.POST,
@@ -108,3 +114,54 @@ def test_confirm_upload_failed():
         _confirm_upload('http://localhost:5000/builds/1', 'token')
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url == url
+
+
+def test_upload_via_keeper(demo_manifest, mocker):
+    """Test upload_via_keeper.
+
+    The goal is to ensure that it
+
+    1. registers a build
+    2. Calls upload with the right attributes
+    3. Confirms the upload
+    """
+    build_resource = {
+        "bucket_name": "an-s3-bucket",
+        "bucket_root_dir": "lsst_apps/builds/b1",
+        "date_created": "2016-03-01T10:21:27.583795Z",
+        "date_ended": None,
+        "git_refs": ["master"],
+        "github_requester": "jonathansick",
+        "product_url": "http://localhost:5000/products/lsst_apps",
+        "self_url": "http://localhost:5000/builds/1",
+        "slug": "b1",
+        "surrogate_key": "35d7a50a1d1b40ab9e7a56cd169f356e",
+        "uploaded": False}
+    mock_register = mocker.patch('ltdmason.uploader._register_build')
+    mock_register.return_value = build_resource
+    mock_upload = mocker.patch('ltdmason.uploader.upload')
+    mock_confirm = mocker.patch('ltdmason.uploader._confirm_upload')
+
+    mock_product = mock.MagicMock()
+    mock_product.html_dir = '_build/html'
+
+    upload_via_keeper(demo_manifest, mock_product,
+                      'https://ltd-keeper.example.org', 'token')
+
+    # Check that upload_via_keeper called the right things
+    assert mock_register.call_count == 1
+    assert mock_upload.call_count == 1
+    assert mock_confirm.call_count == 1
+
+    mock_register.assert_called_once_with(
+        demo_manifest, 'https://ltd-keeper.example.org', 'token')
+
+    mock_upload.assert_called_once_with(
+        build_resource['bucket_name'],
+        build_resource['bucket_root_dir'],
+        mock_product.html_dir,
+        surrogate_key=build_resource['surrogate_key'],
+        acl='public-read',
+        cache_control_max_age=31536000)
+
+    mock_confirm.assert_called_once_with(build_resource['self_url'], 'token')
